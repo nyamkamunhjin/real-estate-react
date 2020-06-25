dd49bf80b6c67d3562e2e53e0b72fc001601f156
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2020-06-25 10:53:51 +0800

    added google maps

--- src/pages/buy/Buy.js
+++ src/pages/buy/Buy.js
@@ -1,25 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 import GridView from "../../components/GridView/GridView";
-
 import "./Buy.css";
 import data from "../../fakeData.json";
 
-class Buy extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="buy-container">
-        <div className="map">
-          <h1>hello from map</h1>
-        </div>
-        <GridView info={data} />
-      </div>
-    );
-  }
-}
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+const center = {
+  lat: 47.918729,
+  lng: 106.917653,
+};
 
-export default Buy;
+export default function Buy() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading maps";
+
+  return (
+    <div className="buy-container">
+      <div className="map">
+        <h2>Maps</h2>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={14}
+          center={center}
+        ></GoogleMap>
+      </div>
+      <GridView info={data} />
+    </div>
+  );
+}
